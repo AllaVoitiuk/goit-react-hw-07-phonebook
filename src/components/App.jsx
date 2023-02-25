a55f7f52c9674bf7a4bdee55c2f26340daeb9d5b
AllaVoitiuk <allavoytjuk@ukr.net>
2023-02-26 00:03:00 +0200

    phonebook with API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import FilterPage from 'pages/FilterPage/FilterPage';
+import  AddContactPage  from 'pages/AddContactPage/AddContactPage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<AddContactPage />} />
+        <Route path="filter" element={<FilterPage />} />
+      </Route>
+    </Routes>
   );
 };
+
+
